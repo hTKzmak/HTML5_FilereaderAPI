6981840f37101835c740c15e9f2530d5e9193e97
Melnik <76968292+hTKzmak@users.noreply.github.com>
2023-11-19 18:44:32 +0300

    Update inputFile.js

--- scripts/inputFile.js
+++ scripts/inputFile.js
@@ -12,8 +12,8 @@ inputFile.addEventListener("change", (e) => {
 
 
     //Проверка на размер файла
-    if ((selectedFile.size / 1024 / 1024) >=10) {
-            return null;
+    if ((selectedFile.size / 1024 / 1024) >= 10) {
+        return null;
     }
 
     const reader = new FileReader();
@@ -23,17 +23,19 @@ inputFile.addEventListener("change", (e) => {
     reader.onload = function () {
         fileName.textContent = selectedFile.name;
         fileType.textContent = selectedFile.type;
-        fileSize.textContent = `${(selectedFile.size / 1024).toFixed(1)} Кбайт`;
+        fileSize.textContent = `${(selectedFile.size / 1024).toFixed(1)} Кбайт`; //toFixed() преобразует число в строку, сохраняя указанное число десятичных знаков
 
-        const fileObject = { 'file_name': fileName.textContent, 
-        'file_type': fileType.textContent, 
-        'file_size': fileSize.textContent, 
-        'file_content': reader.result }
+        const fileObject = {
+            'file_name': fileName.textContent,
+            'file_type': fileType.textContent,
+            'file_size': fileSize.textContent,
+            'file_content': reader.result
+        }
 
 
         //добавление данных о файле в LocalStorage с уникальным ключом
         let newKey = localStorage.length + 1;
-        localStorage.setItem(newKey,  JSON.stringify(fileObject));
+        localStorage.setItem(newKey, JSON.stringify(fileObject));
 
         let link = document.createElement('a');
         link.download = fileName.textContent;
